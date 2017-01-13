93a49034de0745b638e60fa454612126df8c9056
fanlychie <fanlychie@yeah.net>
2017-01-13 17:14:10 +0800

    本地文件

---
+++ src/main/java/org/fanlychie/commons/file/LocalFile.java
@@ -0,0 +1,49 @@
+package org.fanlychie.commons.file;
+
+import java.io.File;
+
+/**
+ * 本地文件
+ * Created by fanlychie on 2017/1/13.
+ */
+public class LocalFile {
+
+    private File file;
+
+    private String key;
+
+    /**
+     * 创建一个本地文件对象
+     *
+     * @param key  表示该文件的 KEY
+     * @param file 本地文件对象
+     */
+    public LocalFile(String key, File file) {
+        this.key = key;
+        this.file = file;
+    }
+
+    /**
+     * 获取本地文件
+     *
+     * @return 返回本地文件对象
+     */
+    public File getFile() {
+        return file;
+    }
+
+    /**
+     * 获取表示本地文件的 KEY
+     *
+     * @return 返回表示本地文件的 KEY
+     */
+    public String getKey() {
+        return key;
+    }
+
+    @Override
+    public String toString() {
+        return "LocalFile{" + "file=" + file + ", key='" + key + '\'' + '}';
+    }
+
+}
