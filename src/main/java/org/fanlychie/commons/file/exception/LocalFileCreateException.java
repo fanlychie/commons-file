2c7e21eaac164b2a9d1daa0dfc0c1a0d2398b7db
fanlychie <fanlychie@yeah.net>
2017-02-22 15:40:36 +0800

    本地文件创建异常

---
+++ src/main/java/org/fanlychie/commons/file/exception/LocalFileCreateException.java
@@ -0,0 +1,13 @@
+package org.fanlychie.commons.file.exception;
+
+/**
+ * 本地文件创建异常
+ * Created by 范忠云 on 2017/2/22.
+ */
+public class LocalFileCreateException extends RuntimeException {
+
+    public LocalFileCreateException(String message) {
+        super(message);
+    }
+
+}
